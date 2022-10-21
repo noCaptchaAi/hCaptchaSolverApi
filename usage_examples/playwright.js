a1bd89603eacbd1dcd8cdc501d9d95d35389db80
hykii <101494204+hykii@users.noreply.github.com>
2022-10-22 00:28:00 +0900

    Update playwright.js

--- usage_examples/playwright.js
+++ usage_examples/playwright.js
@@ -15,7 +15,7 @@ const solver = {
     function getRandomInt(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
     function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
-    const url = "https://accounts.hcaptcha.com/demo?sitekey=4c672d35-0701-42b2-88c3-78380b0db560";
+    const url = "https://accounts.hcaptcha.com/demo";
 
     const browser = await playwright.chromium.launch({ "headless": false });
     const context = await browser.newContext({ "viewport": null });
@@ -27,34 +27,36 @@ const solver = {
 
     page.on("response", async function (response) {
         if (response.url().startsWith("https://hcaptcha.com/getcaptcha/")) {
+            // console.log("Received hCaptcha response.");
             const resp = await response.json();
             let images = {};
-            let counter = 1;
-            let target = resp.requester_question.en.replace(/Please click each image containing an? /, "");
-            for (const task of resp.tasklist) {
-                images[counter - 1] = await axios.get(task.datapoint_uri, { responseType: "arraybuffer" }).then(response => Buffer.from(response.data, "base64").toString('base64'));
-                counter = counter + 1;
+            let counter = 0;
+            let target = resp.requester_question.en;
+            for (let task of resp.tasklist) {
+                images[counter] = await axios.get(task.datapoint_uri, { responseType: "arraybuffer" }).then(response => Buffer.from(response.data, "base64").toString('base64'));
+                counter++;
+                await sleep(getRandomInt(10, 100));
             }
             await sleep(1000);
             console.log(await solve(images, target));
         }
-    });
-
-    const boxFrame = await page.waitForSelector("[title='widget containing checkbox for hCaptcha security challenge']").then(frame => frame.contentFrame()), boxBB = await boxFrame.waitForSelector("#anchor-wr").then(e => e.boundingBox());
+    }); 
     const site_key = await page.evaluate(() => {
         return document.querySelector("[title='widget containing checkbox for hCaptcha security challenge']").getAttribute("src").split("&sitekey=")[1].split("&")[0];
     });
-    let cursorPos = { "x": 0, "y": 0 };
-    for (const pos of path({ "x": 0, "y": 0 }, { "x": boxBB.x + getRandomInt(0, boxBB.width), "y": boxBB.y + getRandomInt(0, boxBB.height) })) {
-        cursorPos = pos;
-        await page.mouse.move(pos.x, pos.y);
-        await sleep(Math.random() * 25);
-    }
-    await page.mouse.down();
-    await sleep(Math.random() * 100);
-    await page.mouse.up();
-    await sleep(500);
-
+    /*
+        // click element like human
+        const boxFrame = await page.waitForSelector("[title='widget containing checkbox for hCaptcha security challenge']").then(frame => frame.contentFrame()), boxBB = await boxFrame.waitForSelector("#anchor-wr").then(e => e.boundingBox());
+        for (const pos of path({ "x": 0, "y": 0 }, { "x": boxBB.x + getRandomInt(0, boxBB.width), "y": boxBB.y + getRandomInt(0, boxBB.height) })) {
+            await page.mouse.move(pos.x, pos.y);
+            await sleep(Math.random() * 25);
+        }
+        await page.mouse.down();
+        await sleep(Math.random() * 100);
+        await page.mouse.up();
+        await sleep(500);
+    */
+    await page.click("[title='widget containing checkbox for hCaptcha security challenge']");
     async function solve(images, target) {
         const captchaFrame = await page.waitForSelector("[title='Main content of the hCaptcha challenge']").then(frame => frame.contentFrame());
         let res = await axios({
@@ -88,11 +90,41 @@ const solver = {
                 // Polling time 3s for paid user and 5s for free user
                 await sleep(5000);
             }
-            for (var correct of res_.solution) {
-                await captchaFrame.click("body > div.challenge-container > div > div > div.task-grid > div:nth-child(" + (parseInt(correct) + 1) + ")");
-                await sleep(getRandomInt(300, 500));
+            try {
+                if (Object.keys(images).length > 9) {
+                    count = 0;
+                    for (let i = 0; i < 9; i++) {
+                        if (res_.solution.includes(i.toString())) {
+                            await captchaFrame.click("div.task-image:nth-child(" + (parseInt(res_.solution[count]) + 1) + ") > div:nth-child(2)");
+                            await sleep(getRandomInt(300, 500));
+                            count++;
+                        }
+                    }
+                    await captchaFrame.click("[class^='button-submit']");
+                    count = 0;
+                    for (let i = 9; i < 18; i++) {
+                        if (res_.solution.includes(i.toString())) {
+                            await captchaFrame.click("div.task-image:nth-child(" + (parseInt(res_.solution[count]) + 1) + ") > div:nth-child(2)");
+                            await sleep(getRandomInt(300, 500));
+                            count++;
+                        }
+                    }
+                    await captchaFrame.click("[class^='button-submit']");
+
+                } else {
+                    count = 0;
+                    for (let i = 0; i < 9; i++) {
+                        if (res_.solution.includes(i.toString())) {
+                            await captchaFrame.click("div.task-image:nth-child(" + (parseInt(res_.solution[count]) + 1) + ") > div:nth-child(2)");
+                            await sleep(getRandomInt(300, 500));
+                            count++;
+                        }
+                    }
+                    await captchaFrame.click("[class^='button-submit']");
+                }
+            } catch (error) {
+                return "An error occurred. (line 89-124, " + error + ")";
             }
-            await captchaFrame.click(".button-submit");
             count = 0;
             let g_recaptcha_response = "";
             while (true) {
@@ -102,9 +134,9 @@ const solver = {
                 if (count >= 5) return "Failed to Solve the Captcha.";
                 await sleep(1000);
             }
-            return "Solved."
+            return "Solved.";
         } else {
-            if (res.status == "skip") { await captchaFrame.click(".button-submit"); return "Skipped the Captcha."; }
+            if (res.status == "skip") { await captchaFrame.click("[class^='button-submit']"); return "Skipped the Captcha."; }
             return "Failed to Solve the Captcha.";
         }
     }
